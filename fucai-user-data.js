890acd315d0c0a3e62ba85be345cf5a720e0a114
Ruifeng Tang <tang7h@gmail.com>
2016-05-27 18:07:43 +0800

    搭建数据展示表格

---
+++ fucai-user-data.js
@@ -0,0 +1,59 @@
+var app = angular.module('caipiao-admin',[]);
+app.controller('appCtrl', function($scope){
+  $scope.userData = [
+    {
+      id: 1,
+      username: "53010845",
+      passcode: "88745496",
+      Tell: "15887228008",
+      info: "康佳k55",
+      Add: "昆明市盘龙区金州湾米兰宾馆",
+      shijian: "2015年9月15日"
+    },
+    {
+      id: 2,
+      username: "53011693",
+      passcode: "88011769",
+      Tell: "15887228008",
+      info: "康佳k55",
+      Add: "昆明市官渡区子君村经济适用房",
+      shijian: "2015年9月15日"
+    },
+    {
+      id: 3,
+      username: "53010613",
+      passcode: "740809",
+      Tell: "15887228008",
+      info: "康佳k55",
+      Add: "昆明市华龙人家1号门",
+      shijian: "2015年9月15日"
+    },
+    {
+      id: 4,
+      username: "53010187",
+      passcode: "10187",
+      Tell: "15887228008",
+      info: "康佳k55",
+      Add: "昆明市盘龙区田园路101号",
+      shijian: "2015年9月15日"
+    },
+    {
+      id: 5,
+      username: "53010499",
+      passcode: "88361790",
+      Tell: "15887228008",
+      info: "康佳k55",
+      Add: "昆明市斗南花卉老市场B区",
+      shijian: "2015年9月15日"
+    },
+    {
+      id: 6,
+      username: "53010453",
+      passcode: "888999",
+      Tell: "15887228008",
+      info: "康佳自行",
+      Add: "昆明市官渡区宝海路2号附2号",
+      shijian: "2015年9月15日"
+    }
+  ]
+})
